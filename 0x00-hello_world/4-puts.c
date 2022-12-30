b38b26ad3c151d06525224fde970264b1475fc2a
Anfalelterefi <anfal123musa@gmail.com>
2022-12-30 13:52:36 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints a given phrase
- * Return: 0 if exited properly, non-zero otherwise
+ * main -A program that prints a line with puts function 
+ * Return: 0 (Success)
  */
 int main(void)
 {
-
-	puts("\"programming is like building a multilingual puzzle");
-
-	return (0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
