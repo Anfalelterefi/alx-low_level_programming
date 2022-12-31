653b5ba701f4d3acf0a013808672ad02e9d9528c
Anfalelterefi <anfal123musa@gmail.com>
2022-12-30 23:28:43 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - Entry 
  * Return: Always 0
  */
-int main(void)
+int main(voi)
 {
-	puts("\"programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
