c0e87df74cdac3fb253b3c3577aa176e1f6fd29f
Anfalelterefi <anfal123musa@gmail.com>
2022-12-30 21:29:40 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - is the entry point for the program.
+ *main -is the entry point for the program.
  *Return:zero if no error, non-zero value if errors.
  */
 int main(void)
